fdb241585ad951d27079c4a852b0b34a31b4c6cc
murtazavadnagar <vadnagarwalamurtaza@gmail.com>
2023-03-18 00:19:29 +0530

    added minSdk version in app.json

--- App.js
+++ App.js
@@ -13,7 +13,9 @@ export default function App(props) {
   })
   const handleMetaMapClickButton = (props) => {
     //set 3 params clientId (cant be null), flowId, metadata
-    MetaMapRNSdk.showFlow("610b96fb7cc893001b135505", "611101668083a1001b13cc80");
+    // MetaMapRNSdk.showFlow("610b96fb7cc893001b135505", "611101668083a1001b13cc80");
+    let data = {params1: "123456"};
+    MetaMapRNSdk.showFlow("610b96fb7cc893001b135505", "611101668083a1001b13cc80", data);
   }
   return (
     <View style={styles.container}>
@@ -32,3 +34,47 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+// import React, {Component, useEffect} from 'react';
+// import {
+//   NativeModules,
+//   NativeEventEmitter,
+//   Button,
+//   View
+// } from 'react-native';
+
+// // import {
+// //   MetaMapRNSdk,
+// // } from 'react-native-metamap-sdk';
+// import {
+//   MetaMapRNSdk,
+// } from 'react-native-expo-metamap-sdk';
+
+
+// function App(props) {
+
+//     useEffect(() => {
+//         const MetaMapVerifyResult = new NativeEventEmitter(NativeModules.MetaMapRNSdk)
+//         MetaMapVerifyResult.addListener('verificationSuccess', (data) => console.log(data))
+//         MetaMapVerifyResult.addListener('verificationCanceled', (data) => console.log(data))
+//     })
+//     const handleMetaMapClickButton = (props) => {
+
+//             //set 3 params clientId (cant be null), flowId, metadata
+//          var yourMetadata = { "param1": "value1", "param2": "value2" }
+//          MetaMapRNSdk.showFlow("610b96fb7cc893001b135505", "611101668083a1001b13cc80", yourMetadata);
+//       }
+
+//     return (
+//           <View
+//             style={{
+//               flex: 1,
+//               justifyContent: 'center',
+//               alignItems: 'center',
+//               backgroundColor: 'powderblue',
+//             }}>
+//             <Button onPress = {() => handleMetaMapClickButton()}  title="Click here"/>
+//           </View>
+//         );
+// }
+// export default App;
